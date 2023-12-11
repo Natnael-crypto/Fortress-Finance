a50623dfcbbb1212a1fd9c2b4e803c9dc3638dbc
efratanigusie <119038187+efratanigusie@users.noreply.github.com>
2023-12-11 12:20:53 +0300

    Add files via upload

---
+++ FrontEnd/script.js
@@ -0,0 +1,13 @@
+
+  document.getElementById("customer-approval-card").addEventListener("click", function() {
+    window.location.href = "/CustomerAccountApproval.html";
+  });
+  document.getElementById("Account-Detail-card").addEventListener("click", function() {
+    window.location.href = "/AccountDetailList.html";
+  });
+  document.getElementById("customer-Account-List").addEventListener("click", function() {
+    window.location.href = "/CustomerAccountList.html";
+  });
+  document.getElementById("Transaction").addEventListener("click", function() {
+    window.location.href = "/Transacton.html";
+  });
